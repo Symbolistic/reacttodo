b122ed782f72e18671672f7bbd7459de78faf7d2
Symbol <Symbolistic@live.com>
2020-05-19 11:47:24 -0400

    Updates

---
+++ precache-manifest.fc181002cf4a92f7725e26c110049e87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e037b1d08f44f7f2071088510d152c",
+    "url": "/reacttodo/index.html"
+  },
+  {
+    "revision": "f520f6bf9c83e4fbb5c5",
+    "url": "/reacttodo/static/js/2.7a337755.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttodo/static/js/2.7a337755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ee0a3ce846076260d25",
+    "url": "/reacttodo/static/js/main.8e33b1c3.chunk.js"
+  },
+  {
+    "revision": "28b5d5c21e401ac5c009",
+    "url": "/reacttodo/static/js/runtime-main.907bc671.js"
+  }
+]);
